1effcbf5b68d5d9ea874c9d3abcf0fa49db45aad
Aditya Chavan <entp847@gmail.com>
2026-03-31 21:22:39 -0700

    Removed login and cart

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -30,12 +30,12 @@ export const Navbar = () => {
         <li onClick={() => { setMenu("ships") }}><Link style={{ textDecoration: 'none' }} to='/ships'>Ships</Link>{menu ==="ships"?<hr/>:<></>}</li>
         <li onClick={() => { setMenu("others") }}><Link style={{ textDecoration: 'none' }} to='/others'>Others</Link>{menu ==="others"?<hr/>:<></>}</li>
         </ul>
-        <div className="nav-login-cart">
+       {/* <div className="nav-login-cart">
         {localStorage.getItem('auth-token') ? <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('/') }}>Logout</button> : <Link to='/login'><button>Login</button></Link>}
         
         <Link to='/cart'><img src={cart_icon} alt="" /></Link>
           <div className="nav-cart-count">{getTotalCartItems()}</div>
-        </div>
+        </div>*/}
     </div>
   )
 }
